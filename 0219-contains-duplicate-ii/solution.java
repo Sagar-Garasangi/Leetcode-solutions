a643e8e8e38b35d3cd959769b562c2109d598ae4
Sagar-Garasangi <sagargarasangi7@gmail.com>
2026-07-05 13:45:32 +0000

    [LeetCode Sync] Runtime - 25 ms (33.76%), Memory - 76.3 MB (80.39%)

---
+++ 0219-contains-duplicate-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+
+            if(Math.abs(right-left)>k){
+                set.remove(nums[left]);
+                left++; 
+            }
+            if(set.contains(nums[right])){
+                return true;
+            }else
+            set.add(nums[right]);
+        }
+    return false;}
+}
